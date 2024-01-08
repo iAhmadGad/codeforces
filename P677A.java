de37f10b7f3f3653b6f63f58423a61895355128e
iAhmadGad <ahmadasaadgad@gmail.com>
2024-01-09 01:38:42 +0300

    Rename solution

---
+++ P677A.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/*
+ * A - Vanya and Fence
+ */
+
+public class P677A {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		short n = scanner.nextShort(), h = scanner.nextShort(), counter = 0;
+		
+		for(short i = 0; i < n; i++)
+		{
+			short a = scanner.nextShort();
+			if(a > h) counter += 2;
+			else counter++;
+		}
+		
+		scanner.close();
+		System.out.println(counter);
+	}
+}
